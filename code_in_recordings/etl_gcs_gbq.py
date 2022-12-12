a886c8bff273f374cd197cac4427a31ad35640d3
Jeff Hale <jeffmshale@gmail.com>
2022-12-12 14:56:51 -0500

    clean etl more

--- code_in_recordings/etl_gcs_gbq.py
+++ code_in_recordings/etl_gcs_gbq.py
@@ -1,15 +1,14 @@
 # extract data from gcs
-# transform by removing nulls - basic data clearning example
+# transform - basic data cleaning example - fill passenger count nans with 0s
 # load data into gbq
 
 # pip install pandas-gbq pyarrow prefect-gcp['cloud_storage']
-# pyarrow larger, but can compress better than fastparquet
+# pyarrow is larger, but can compress better than fastparquet
 
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp import GcpCredentials
 from prefect_gcp.cloud_storage import GcsBucket
-from prefect_gcp.bigquery import bigquery_load_file
 from google.oauth2 import service_account
 
 gcp_credentials_block = GcpCredentials.load("de-zoom-auth")
@@ -21,7 +20,7 @@ def transform(
 ) -> pd.DataFrame:  # simplified data cleaning example
     df_raw = pd.read_parquet(path)
     print(f'pre: missing passenger counts: {df_raw["passenger_count"].isna().sum()}')
-    df_raw["passenger_count"] = df_raw["passenger_count"].fillna(0)  # fill with 0
+    df_raw["passenger_count"] = df_raw["passenger_count"].fillna(0)
     print(f'post: missing passenger counts: {df_raw["passenger_count"].isna().sum()}')
     return df_raw
 
@@ -31,7 +30,7 @@ def write_bq(df: pd.DataFrame) -> None:
     try:
         df.to_gbq(
             destination_table="prefect-sbx-community-eng.dezoomcamp.rides",
-            project_id="prefect-sbx-community-eng",  # dataset id ok?
+            project_id="prefect-sbx-community-eng",
             credentials=service_account.Credentials.from_service_account_info(
                 gcp_credentials_block.service_account_info
             ),
@@ -51,11 +50,13 @@ def etl():
     gcs_path = f"{color}/{color}_2022_09.parquet"
 
     gcs_block = GcsBucket.load("gcs-best")
-    gcs_block.get_directory(  # also will just get a file and writes it out, no return
-        from_path=gcs_path, local_path="./"
-    )
+    gcs_block.get_directory(from_path=gcs_path, local_path="./")
+    # will get a file and write it out, no return
 
+    # transform
     df = transform(gcs_path)  # same as local path
+
+    # load
     write_bq(df)
 
 
