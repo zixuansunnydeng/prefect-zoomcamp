aa3b891284cf223cbc6553b49a515d245d91caf8
Jeff Hale <jeffmshale@gmail.com>
2022-12-12 16:23:23 -0500

    more comment cleaning

--- code_in_recordings/el_gcs.py
+++ code_in_recordings/el_gcs.py
@@ -1,52 +1,67 @@
-# fetch data from web
-# inspect
-# write data locally
-# Write to gcs bucket
-
 import random
-import pandas as pd  # install pandas and pyarrow, larger, but can compress better than fastparquet
+from pathlib import Path
+import pandas as pd
 from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
 
 
-@task(retries=3)
-def extract() -> pd.DataFrame:  # later parametrize with month and year and color
-    # read data into pandas dataframe
+@task(retries=3, log_prints=True)
+def extract() -> pd.DataFrame:
+    """Read data from web into pandas dataframe and inspect"""
 
     # create artificial failure to show value of retries
     # if random.randint(0, 1) > 0:
     #     raise Exception
+
     df = pd.read_parquet(
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-09.parquet"
     )
-    print(df.head(2))  # log_prints shows in terminal and ui
+    print(df.head(2))
     print(f"columns: {df.columns}")
     print(f"rows: {len(df)}")
     return df
 
 
 @task()
-def write_local(df: pd.DataFrame) -> str:
+def write_local(df: pd.DataFrame) -> Path:
+    """Write out DataFrame to local parquet file"""
     color = "yellow"
     year = "2022"
     month = "09"
-    path = f"{color}/{color}_{year}_{month}.parquet"
-    df.to_parquet(
-        path, compression="gzip"
-    )  # default is snappy compression, not as much compression as gzip
-    # pyarrow is used by default, if available. compresses better, so make sure available
+    path = Path(f"{color}/{color}_{year}_{month}.parquet")
+    df.to_parquet(path, compression="gzip")
+    # default is snappy compression, not as much compression as gzip
+    # pyarrow is used by default, if available
+    # pyarrow compresses better than fastparquet, but is larger pkg
     return path
 
 
-@flow()
-def el3() -> None:
-    from prefect_gcp.cloud_storage import GcsBucket
+@task()
+def write_gcs(color: str) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("gcs-best")
+    gcs_block.put_directory(local_path=color, to_path=color)
+    return
 
+
+@task()
+def cleanup(path: Path) -> None:
+    """Delete the file locally after use"""
+    path.unlink(missing_ok=True)
+    return
+
+
+@flow()
+def el() -> None:
+    """The main extract and load function"""
+    color = "yellow"  # taxi color
     df = extract()
     path = write_local(df)
-    color = "yellow"
-    gcs_block = GcsBucket.load("gcs-best")
-    gcs_block.put_directory(local_path=f"{color}", to_path="yellow")
+    write_gcs(color)
+    cleanup(path)
+
+    return
 
 
 if __name__ == "__main__":
-    el3()
+    el()
