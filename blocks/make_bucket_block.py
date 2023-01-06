dbf135a7d741285938e816964a69b8a29e20de5f
Jeff Hale <jeffmshale@gmail.com>
2023-01-05 20:22:13 -0500

    rename block files

--- blocks/make_bucket_block.py
+++
@@ -1,9 +0,0 @@
-from prefect_gcp import GcpCredentials
-from prefect_gcp.cloud_storage import GcsBucket
-
-bucket_block = GcsBucket(
-    gcp_credentials=GcpCredentials.load("zoomcamp-gcp-creds-block"),
-    bucket="prefect-de-zoomcamp",
-)
-
-bucket_block.save("zoomcamp-gcs-bucket-block", overwrite=True)
