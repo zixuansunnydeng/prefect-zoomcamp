1a473a4ca9394238ff9698f18ecbd6ba5779113c
Jeff Hale <jeffmshale@gmail.com>
2023-01-08 13:51:20 -0500

    ad auto remove to docker block

--- blocks/make_docker_block.py
+++ blocks/make_docker_block.py
@@ -2,7 +2,9 @@ from prefect.infrastructure.docker import DockerContainer
 
 # alternative to creating DockerContainer block in the UI
 docker_block = DockerContainer(
-    image="discdiver/prefect:zoom", image_pull_policy="ALWAYS"  # insert your image here
+    image="discdiver/prefect:zoom",  # insert your image here
+    image_pull_policy="ALWAYS",
+    auto_remove=True,
 )
 
 docker_block.save("zoom", overwrite=True)
